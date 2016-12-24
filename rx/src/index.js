710b2ed95ba41a17ac701609f58248799f9f9a77
森田賢二 <kenjimoritata@gmail.com>
2016-12-24 17:41:37 +0900

    rx env

---
+++ rx/src/index.js
@@ -0,0 +1,35 @@
+// @flow
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import 'babel-polyfill';
+import Rx from 'rx';
+import fetch from 'isomorphic-fetch';
+
+class Greet extends Component {
+ render() {
+    var source =  Rx.Observable.timer(200, 100)
+         .timerInterval()
+         .pluck('interval')
+         .take(3);
+    var subscription = source.subscribe(
+        function (x) {
+           console.log(x);
+        },
+        function (error) {
+           console.log('faError')
+        },
+        function () {
+            conswole.log('Comp')
+        }
+    )
+
+  return (
+   <ul id='result'>
+      </ul>
+  )
+ }
+}
+window.addEventListener('DOMContentLoaded', ()=>{
+ ReactDOM.render(<Greet />, document.querySelector('main'));
+})
+
